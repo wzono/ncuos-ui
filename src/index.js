22e819e973fd793dbf0e9f0d9cf516bef385977a
lenconda <1053464288@qq.com>
2018-03-21 09:15:54 +0800

    添加src/index.js

---
+++ src/index.js
@@ -0,0 +1,25 @@
+// 在这里import组件
+import {} from '';
+
+// 在这里声明组件
+const components = {};
+
+// 组件库名称
+const nos = {...components};
+
+// Vue插件安装命令
+const install = function (Vue, opts = {}) {
+  // 暂时没弄清楚这条语句是什么意思
+  // if (install.installed) return
+  Object.keys(nos).forEach(key => {
+    Vue.component(key, nos[key]);
+    // 在下面添加命令组件
+  });
+};
+
+// istanbul ignore if
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue);
+}
+
+module.exports.default = module.exports;
