8036cf3f21e783616849a705e17c69b501d742c9
lenconda <1053464288@qq.com>
2018-03-21 16:51:49 +0800

    修改组件所在目录，修改index.js

--- examples/App.vue
+++ examples/App.vue
@@ -18,5 +18,4 @@ export default {
   color: #2c3e50;
   padding: 50px;
 }
-  
 </style>
--- packages/button/src/button.vue
+++ packages/button/src/button.vue
@@ -5,7 +5,7 @@
 </template>
 <script>
 export default {
-  name: "Ncuosbutton",
+  name: "us-button",
   props: {
     type: {
       type: String,
