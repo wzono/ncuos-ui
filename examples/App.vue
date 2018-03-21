bcc89d562179941eca4b97fcb62a16ed497e2a7b
lenconda <1053464288@qq.com>
2018-03-21 21:37:39 +0800

    添加了一些sass，构建了两个测试页面

--- examples/App.vue
+++ examples/App.vue
@@ -12,10 +12,6 @@ export default {
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
   padding: 50px;
 }
 </style>
---
+++ examples/pages/Layout.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="hello">
+    <h1>布局测试页面</h1>
+    <hr>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Layout',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" type="text/scss">
+.hello {
+  text-align: center;
+  * {
+    box-sizing: border-box;
+  }
+  h1, h2 {
+    font-weight: normal;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+    li {
+      display: inline-block;
+      margin: 0 10px;
+      a {
+        color: #42b983;
+      }
+    }
+  }
+  .card {
+    width: 100%;
+    border: 1px solid #ebeef5;
+    border-radius: 4px;
+    box-shadow: 0 2px 12px 0 rgba(0, 0, 0, .1);
+    padding: 18px 20px;
+    text-align: left;
+    margin: 24px 0;
+    .header {
+      padding: 18px 20px;
+      border-bottom: 1px solid #ebeef5;
+      box-sizing: border-box;
+      font-weight: 700;
+    }
+    .body {
+      height: auto;
+      padding: 18px;
+      padding-left: 20px;
+    }
+  }
+}
+</style>
