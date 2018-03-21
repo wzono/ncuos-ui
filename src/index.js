14088f164e40274ce706bac337bdf7e442c07f3b
wingsico <wingsico@outlook.com>
2018-03-21 23:20:03 +0800

    删除了一点分号

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 // 在这里import组件
-import {Usbutton} from '../packages/button';
+import {Usbutton} from '../packages/button'
 
 // 在这里声明组件
 const components = [
   Usbutton
-];
+]
 
 // Vue插件安装命令
 const install = function (Vue, opts = {}) {
   components.map(component => {
-    Vue.component(component.name, component);
+    Vue.component(component.name, component)
     // 以下是服务组件
     // Vue.prototype.$loading = Loading.service;
     // Vue.prototype.$msgbox = MessageBox;
@@ -17,13 +17,13 @@ const install = function (Vue, opts = {}) {
     // Vue.prototype.$confirm = MessageBox.confirm;
     // Vue.prototype.$prompt = MessageBox.prompt;
     // Vue.prototype.$notify = Notification;
-    // Vue.prototype.$message = Message; 
-  });
-};
+    // Vue.prototype.$message = Message;
+  })
+}
 
 // istanbul ignore if
 if (typeof window !== 'undefined' && window.Vue) {
-  install(window.Vue);
+  install(window.Vue)
 }
 
-module.exports.default = module.exports;
+module.exports.default = module.exports
