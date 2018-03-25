1ea9b782aa1b51fe71acbfa51084f9d6f0e3acc7
stan1812 <2314980309@qq.com>
2018-03-25 22:15:50 +0800

    init button feature

--- packages/button/src/button.vue
+++ packages/button/src/button.vue
@@ -1,22 +1,37 @@
 <template>
-  <button>
+  <button @click="$emit('click')" class="us-button" :disabled="disabled" :class="[
+    'us-button--'+type,
+    {'is-plain': plain, 'is-disabled': disabled},
+    'us-button--size-'+size
+    ]">
+    <!-- <i v-if="icon !== ''" :class="icon"></i> -->
     <slot></slot>
   </button>
 </template>
-
 <script>
 export default {
-  name: 'us-button',
+  name: "us-button",
   props: {
     type: {
       type: String,
-      default: 'default'
-    }
+      default: "default"
+    },
+    size: {
+      type: String,
+      default: "default"
+    },
+    icon: {
+      type: String,
+      default: ""
+    },
+    plain: Boolean,
+    disabled: Boolean,
+    round: Boolean
   },
   data() {
     return {
-
-    }
+      msg: "button"
+    };
   }
-}
+};
 </script>
