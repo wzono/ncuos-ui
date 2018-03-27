2480fc4e003e441453118b991fa8be852613ff17
lenconda <1053464288@qq.com>
2018-03-27 19:45:41 +0800

    [Message]美化组件，但是还有很多bug

--- packages/message/src/message.vue
+++ packages/message/src/message.vue
@@ -1,11 +1,18 @@
 <template>
-  <transition>
-    <div class="us-message" v-show="show">
-      <div class="us-message__icon" :class="[`icon__${type}`]"></div>
-      <div class="us-message__content">
-        <slot>
-          {{ message }}
-        </slot>
+  <transition name="us-messagebox">
+    <div class="message">
+      <div class="us-message" v-show="show">
+        <div class="us-message__content">
+          <slot>
+            <i v-if="type !== 'default'" class="fa icon" :class="[
+            {'fa-check-circle': type === 'success'},
+            {'fa-info-circle': type === 'info'},
+            {'fa-exclamation-circle': type === 'warning'},
+            {'fa-times-circle': type === 'error'}, `icon__${type}`]"></i>
+            <span>{{ message }}</span>
+          </slot>
+        </div>
+        <div class="us-message__close" @click="show = false">&times;</div>
       </div>
     </div>
   </transition>
@@ -32,9 +39,11 @@ export default {
       if (this.timer) {
         clearTimeOut(this.timer)
       } else {
-        this.timer = setTimeout(() => {
-          this.show = false
-        }, this.duration)
+        if (this.duration !== 0) {
+          this.timer = setTimeout(() => {
+            this.show = false
+          }, this.duration === 0 ? '' : '')
+        }
       }
     }
   }
